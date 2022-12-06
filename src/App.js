d1943fec3ebcc5c347549a6996afaed1618a4611
= <=>
2022-12-06 16:33:34 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({});
+
+  const fetchData = async () => {
+    const username = "C917BD7916D242A48F48";
+    const password = "T1hqLexGYuxXPMdr-vSvxx3dTmjVrHMqHT5MEOfq";
+
+    const options = {
+      headers: {
+        Authorization: `Basic ${btoa(`${username}:${password}`)}`,
+      },
+    };
+    const response = await fetch(
+      "https://api.ravelry.com/current_user.json",
+      options
+    );
+    const json = await response.json();
+    setCurrentUser({ ...currentUser, ...json });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const formatData = () => {
+    const { username, large_photo_url } = currentUser.user;
+    return (
+      <>
+        <h1>{username}</h1> <img src={large_photo_url} alt="profile picture" />
+      </>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Object.keys(currentUser).length && formatData(currentUser)}
     </div>
   );
 }
