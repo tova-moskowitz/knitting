a5fc114bd485d1a731b3be6bb4b475c873ea6c4a
= <=>
2022-12-13 12:17:19 -0500

    fetch user and projects

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import "./App.css";
 
 function App() {
   const [currentUser, setCurrentUser] = useState({});
+  const [currentUserProjects, setCurrentUserProjects] = useState({});
 
   const fetchData = async () => {
     const username = "C917BD7916D242A48F48";
     const password = "T1hqLexGYuxXPMdr-vSvxx3dTmjVrHMqHT5MEOfq";
+    const ravUserName = "GoodRebecca";
 
     const options = {
       headers: {
         Authorization: `Basic ${btoa(`${username}:${password}`)}`,
       },
     };
-    const response = await fetch(
+
+    const users = await fetch(
       "https://api.ravelry.com/current_user.json",
       options
     );
-    const json = await response.json();
-    setCurrentUser({ ...currentUser, ...json });
+    const usersJSON = await users.json();
+
+    const projects = await fetch(
+      `https://api.ravelry.com/projects/${ravUserName}/list.json`,
+      options
+    );
+    const projectsJSON = await projects.json();
+
+    await Promise.all([usersJSON, projectsJSON]);
+    setCurrentUser(usersJSON.user);
+    setCurrentUserProjects(projectsJSON.projects);
   };
 
   useEffect(() => {
@@ -26,17 +38,26 @@ function App() {
   }, []);
 
   const formatData = () => {
-    const { username, large_photo_url } = currentUser.user;
-    return (
-      <>
-        <h1>{username}</h1> <img src={large_photo_url} alt="profile picture" />
-      </>
-    );
+    return currentUserProjects.map((project) => {
+      return (
+        <>
+          <h1>
+            <a href={project.links.self.href} target="_blank">
+              {project.name}
+            </a>
+          </h1>
+          <img src={project?.first_photo?.small_url} />
+        </>
+      );
+    });
   };
+  const { username, large_photo_url } = currentUser;
 
   return (
     <div className="App">
-      {Object.keys(currentUser).length && formatData(currentUser)}
+      {username}
+      <img src={large_photo_url} />
+      {Object.keys(currentUser).length && formatData()}
     </div>
   );
 }
