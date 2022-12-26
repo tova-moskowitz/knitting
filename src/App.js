3c967a6eb2051f37da884aa0674461aee8b8d1e6
= <=>
2022-12-26 17:01:26 -0500

    pull details about a user and display. With media query too

--- src/App.js
+++ src/App.js
@@ -4,32 +4,49 @@ import "./App.css";
 function App() {
   const [currentUser, setCurrentUser] = useState({});
   const [currentUserProjects, setCurrentUserProjects] = useState({});
+  const {
+    username,
+    large_photo_url,
+    first_name,
+    location,
+    about_me,
+    fave_curse,
+  } = currentUser;
 
   const fetchData = async () => {
     const username = "C917BD7916D242A48F48";
     const password = "T1hqLexGYuxXPMdr-vSvxx3dTmjVrHMqHT5MEOfq";
-    const ravUserName = "GoodRebecca";
-
+    const ravUserName = "goodrebecca";
+    // const ravUserName = "sockdiva";
+    // const ravUserName = "yarnzilla";
     const options = {
       headers: {
         Authorization: `Basic ${btoa(`${username}:${password}`)}`,
       },
     };
 
-    const users = await fetch(
-      "https://api.ravelry.com/current_user.json",
+    const user = await fetch(
+      `https://api.ravelry.com/people/${ravUserName}.json`,
       options
     );
-    const usersJSON = await users.json();
+    const userJSON = await user.json();
 
+    // /current_user.json
+    // const users = await fetch(
+    //   "https://api.ravelry.com/current_user.json",
+    //   options
+    // );
+    // const usersJSON = await users.json();
+
+    // /projects/{id}.json}
     const projects = await fetch(
       `https://api.ravelry.com/projects/${ravUserName}/list.json`,
       options
     );
     const projectsJSON = await projects.json();
 
-    await Promise.all([usersJSON, projectsJSON]);
-    setCurrentUser(usersJSON.user);
+    await Promise.all([userJSON, projectsJSON]);
+    setCurrentUser(userJSON.user);
     setCurrentUserProjects(projectsJSON.projects);
   };
 
@@ -37,27 +54,52 @@ function App() {
     fetchData();
   }, []);
 
-  const formatData = () => {
-    return currentUserProjects.map((project) => {
-      return (
-        <>
-          <h1>
-            <a href={project.links.self.href} target="_blank">
-              {project.name}
-            </a>
-          </h1>
-          <img src={project?.first_photo?.small_url} />
-        </>
-      );
-    });
-  };
-  const { username, large_photo_url } = currentUser;
-
   return (
     <div className="App">
-      {username}
-      <img src={large_photo_url} />
-      {Object.keys(currentUser).length && formatData()}
+      <div className="user-info">
+        <h1>{username}</h1>
+        <p className="user-first-name">{first_name}</p>
+        <div className="user-details">
+          <img src={large_photo_url} />
+          <div className="inner-details">
+            <p>
+              <span>Location:</span> {location}
+            </p>
+            <p>
+              <span>About Me:</span> {about_me}
+            </p>
+            <p>
+              <span>Favorite Curse:</span> {fave_curse}
+            </p>
+          </div>
+        </div>
+      </div>
+      <ul className="projects-list">
+        {Object.keys(currentUser).length &&
+          currentUserProjects.length &&
+          currentUserProjects.map((project) => {
+            return (
+              <>
+                {/* {
+                  <li className={`single-project id-${project.id}`}>
+                    <a
+                      className="link"
+                      href={project.links.self.href}
+                      target="_blank"
+                    >
+                      {project.name}
+                    </a>
+                    <img
+                      className="project-photo"
+                      src={project?.first_photo?.small_url}
+                      alt={project.name}
+                    />
+                  </li>
+                } */}
+              </>
+            );
+          })}
+      </ul>
     </div>
   );
 }
